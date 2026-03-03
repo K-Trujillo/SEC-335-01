99f546115066eefd3ae2a27cb4e0cf99ec110b14
Kristel <kristel.trujillo@mymail.champlain.edu>
2026-03-03 12:38:50 -0500

    Re-added hydrahttp.sh, adjusted mangler.sh

--- week5/mangler.sh
+++ week5/mangler.sh
@@ -14,4 +14,5 @@ clear='\033[0m'
 
 echo -e "${green}Mangling ${f1} to ${f2}. Min: ${m}, Max: ${x} ${clear}"
 touch "${f2}"
-rsmangler -f "$f1" -m "$m" -x "$x" -o "$f2" | cat "$f2" | tail "$f2" -n 5
+rsmangler --file "$f1" -m "$m" -x "$x" > "$f2" 
+head "$f2" -n 5
