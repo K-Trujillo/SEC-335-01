ed73b6e65b734980d37afe32611cb4556c518fcf
Kristel <kristel.trujillo@mymail.champlain.edu>
2026-03-03 11:56:40 -0500

    testing new gitignore

--- week5/mangler.sh
+++ week5/mangler.sh
@@ -13,4 +13,5 @@ green='\033[0;32m'
 clear='\033[0m'
 
 echo -e "${green}Mangling ${f1} to ${f2}. Min: ${m}, Max: ${x} ${clear}"
+touch "${f2}"
 rsmangler -f "$f1" -m "$m" -x "$x" -o "$f2" | cat "$f2" | tail "$f2" -n 5
